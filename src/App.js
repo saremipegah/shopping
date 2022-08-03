f2925bd53cdd1eaa6363813fa77037e42cc36526
saremipegah <saremipegah@gmail.com>
2022-08-03 12:37:37 +0430

    shopping

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import ProductsSection from "./Components/ProductsSection/ProductsSection";
+import "./App.css";
+import products from "./data/products";
+import Toast from "./Components/Toast/Toast";
+import Cart from "./Components/Cart/Cart";
+import productsContext from "./Components/Contexts/ProductsContext";
 
-function App() {
+export default function App() {
+  const [allProducts, setAllProducts] = useState(products);
+  const [userCart , setUserCart]= useState([])
+  const [isShowToast,setIsShowToast]= useState(false)
+  const [isShowCart,setIsShowCart]= useState(false )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <productsContext.Provider value={{
+        allProducts,
+        userCart,
+        setUserCart,
+        isShowToast,
+        setIsShowToast,
+        isShowCart,
+        setIsShowCart
+
+      }}>
+        <Navbar />
+        <main className="pb-5">
+          <div className="container">
+            <h1 className="text-center main-title">All Products</h1>
+
+            {allProducts.map(() => (
+              <ProductsSection  />
+            ))}
+          </div>
+        </main>
+        <Toast />
+        <Cart />
+      </productsContext.Provider>
     </div>
   );
 }
-
-export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import {BsBag} from 'react-icons/bs'
+import productsContext from '../Contexts/ProductsContext'
+import './Navbar.css'
+
+export default function Navbar() {
+
+    const contextData = useContext(productsContext) 
+  return (
+    <nav className='navbar navbar-expand-sm py-3 d-flex'>
+        <div className="container">
+            <a href="#" className="navbar-brand">
+                GADA Shop
+            </a>
+            <ul className="navbar-nav me-auto ms-3">
+                <li className="nav-item">
+                    <a href="" className="nav-link">
+                        Home
+                    </a>
+                </li>
+            </ul>
+            <div className="bag-box">
+                <a href="#" className='bag'>
+                <BsBag className="text-white" onClick={() => contextData.setIsShowCart(true)}/>
+                    <span className='bag-products-counter'>0</span>
+
+                </a>
+            </div>
+        </div>
+    </nav>
+  )
+}
